1adbb306c25879bf2f2bfe889f0b1f9cc79f67fd
sumeetby16 <sumeetby16@gmail.com>
2025-12-03 11:16:03 +0530

    Add script to calculate sum and average of scores

---
+++ sumandavg.py
@@ -0,0 +1,24 @@
+# array_scores.py
+
+import sys
+
+if len(sys.argv)>1:
+  script_name=sys.argv[0]
+  scores = [int(arg) for arg in sys.argv[1:]]
+  print("user details inputted.")
+
+else:
+    print("default details.")
+    script_name=sys.argv[0]
+    scores=[10,20,30,40]
+    
+total = sum(scores)
+average = total / len(scores)
+maximum = max(scores)
+minimum = min(scores)
+    
+print(f"Scores: {scores}")
+print(f"Sum of scores: {total}")
+print(f"Average of scores: {average:.2f}")
+print(f"Maximum score: {maximum}")
+print(f"Minimum score: {minimum}")
